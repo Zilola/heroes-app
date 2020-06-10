48804e4b87d3aa07a6c61b959a4258c2b18fb521
Zilola Matasimova <zilola.matasimova@gmail.com>
2020-06-09 23:29:36 -0400

    service inMemoryData

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,25 +3,34 @@ import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Injectable } from '@angular/core';
 import {Observable, of} from 'rxjs';
+import {HttpClient, HttpHeader} from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
- // getHeroes(): Hero[]{
+  // getHeroes(): Hero[]{
    // return HEROES;
-  //}
+   //}
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    //this.messageService.add('HeroService: fetched heroes');
+    //return of(HEROES);
+    return this.htpp.get<Hero[]>(this.heroesUrl);
   }
-
+  
   getHero(id:number): Observable <Hero>{
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero =>hero.id === id));
   }
+  
 
-
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private htpp: HttpClient) { }
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`)
+  }
+  private heroesUrl = 'api/heroes'; // Url to the web api
 }
+
